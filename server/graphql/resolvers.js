4fdb839ee1637d12e22f5f6ab8b329c7a1d486ae
Sprite <ponkaew.wasin@gmail.com>
2022-04-12 17:12:50 -0500

    new field

--- server/graphql/resolvers.js
+++ server/graphql/resolvers.js
@@ -13,4 +13,30 @@ module.exports = {
         },
     },
 
+    Mutation: {
+        register: async (parent, args) => {
+            let {username, email, password, confirmPassword} = args;
+
+            try {
+
+                // Validate input data
+
+                // Check if user/email exists
+
+                // Hash pass
+                // #3 14:32 npm install bcryptjs -- will do later if neccessary
+
+                // Create User
+                const user = await User.create({
+                    username, email, password
+                });
+                // Return User
+                return user;
+            } catch(err){
+                console.log(err);
+                throw err;
+            }
+        }
+    }
+
 };
--- server/graphql/typeDefs.js
+++ server/graphql/typeDefs.js
@@ -1,13 +1,21 @@
 const { gql } = require('apollo-server')
 
 module.exports = gql`
-type User {
-    username: String!
-    email: String!
-}
+    type User {
+        username: String!
+        email: String!
+    }
 
-type Query {
-    getUsers: [User]!
-}
+    type Query {
+        getUsers: [User]!
+    }
 
+    type Mutation{
+        register(
+            username: String!,
+            email:String!, 
+            password: String!, 
+            confirmPassword: String!
+        ): User! 
+    }
 `;
