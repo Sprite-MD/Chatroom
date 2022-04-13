51406857d869ee51775c990b96244c60628fd8fc
Sprite <ponkaew.wasin@gmail.com>
2022-04-12 23:04:03 -0500

    added user/email validation

--- server/graphql/resolvers.js
+++ server/graphql/resolvers.js
@@ -1,4 +1,6 @@
+const { formatApolloErrors } = require('apollo-server-errors');
 const { User } = require('../models')
+const {UserInputError} = require('apollo-server')
 
 module.exports = {
     Query: {
@@ -16,13 +18,28 @@ module.exports = {
     Mutation: {
         register: async (parent, args) => {
             let {username, email, password, confirmPassword} = args;
+            let error = {}
 
             try {
 
                 // Validate input data
+                if (email.trim() === '') error.email = 'Email must not be empty';
+                if (username.trim() === '') error.username = 'Username must not be empty';
+                if (password.trim() === '') error.password = 'Password must not be empty';
+                if (confirmPassword.trim() === '') error.confirmPassword = 'repeat password must not be empty';
+
+                if (password != confirmPassword) error.confirmPassword = 'Password must match'
 
                 // Check if user/email exists
+                const userByUsername = await User.findOne({ where: { username }});
+                const userByEmail = await User.findOne({ where: { email }});
 
+                if (userByUsername) error.username = "Username is taken";
+                if (userByEmail) error.email = "Email is taken";
+
+                if (Object.keys(error).length  > 0){
+                    throw error
+                };
                 // Hash pass
                 // #3 14:32 npm install bcryptjs -- will do later if neccessary
 
@@ -30,11 +47,12 @@ module.exports = {
                 const user = await User.create({
                     username, email, password
                 });
+
                 // Return User
                 return user;
             } catch(err){
                 console.log(err);
-                throw err;
+                throw new UserInputError("Bad input", { error: err });
             }
         }
     }
