6e62832f9ea40d214889e7caaccfb3deda4a764e
Sprite <ponkaew.wasin@gmail.com>
2022-04-15 16:29:35 -0500

    added login system

--- server/graphql/resolvers.js
+++ server/graphql/resolvers.js
@@ -1,6 +1,7 @@
 const { formatApolloErrors } = require('apollo-server-errors');
-const { User } = require('../models')
-const {UserInputError} = require('apollo-server')
+const { User } = require('../models');
+const {UserInputError, AuthenticationError} = require('apollo-server');
+const bcrypt = require('bcryptjs');
 
 module.exports = {
     Query: {
@@ -13,22 +14,49 @@ module.exports = {
                 console.log(err);
             }
         },
+
+        login: async (parent, args) => {
+            const { username, password } = args;
+            let errors = {}
+
+            try{
+                const user = await User.findOne({
+                    where: { username }
+                });
+
+                if (!user){
+                    errors.username = 'User not found';
+                    throw new UserInputError('User not found', { errors });
+                }
+
+                const correctPassword = await bcrypt.compare(password, user.password)
+                if (!correctPassword){
+                    errors.password = 'Password is incorrect'
+                    throw new AuthenticationError('Password is incorrect', {errors})
+                }
+
+                return user
+            }catch(err){
+                console.log(err)
+                throw err
+            };
+        },
     },
 
     Mutation: {
         register: async (parent, args) => {
             let {username, email, password, confirmPassword} = args;
-            let error = {};
+            let errors = {};
 
             try {
 
                 // Validate input data
-                if (email.trim() === '') error.email = 'Email must not be empty';
-                if (username.trim() === '') error.username = 'Username must not be empty';
-                if (password.trim() === '') error.password = 'Password must not be empty';
-                if (confirmPassword.trim() === '') error.confirmPassword = 'repeat password must not be empty';
+                if (email.trim() === '') errors.email = 'Email must not be empty';
+                if (username.trim() === '') errors.username = 'Username must not be empty';
+                if (password.trim() === '') errors.password = 'Password must not be empty';
+                if (confirmPassword.trim() === '') errors.confirmPassword = 'repeat password must not be empty';
 
-                if (password != confirmPassword) error.confirmPassword = 'Password must match'
+                if (password != confirmPassword) errors.confirmPassword = 'Password must match'
 
                 // // Check if user/email exists
                 // const userByUsername = await User.findOne({ where: { username }});
@@ -37,11 +65,11 @@ module.exports = {
                 // if (userByUsername) error.username = "Username is taken";
                 // if (userByEmail) error.email = "Email is taken";
 
-                if (Object.keys(error).length  > 0){
-                    throw error;
+                if (Object.keys(errors).length  > 0){
+                    throw errors;
                 };
                 // Hash pass
-                // #3 14:32 npm install bcryptjs -- will do later if neccessary and import
+                // #3 14:32 npm install bcryptjs 
                 // password = await bcrypt.hash(password, 6);
 
                 // Create User
@@ -56,12 +84,13 @@ module.exports = {
                 console.log(err);
                 if (err.name === 'SequelizeUniqueConstraintError'){
                     err.errors.forEach( 
-                        (e) => (error[e.path] = `${e.path} is already taken`)
+                        (e) => (errors[e.path] = `${e.path} is already taken`)
                     )
+                    
                 } else if (err.name === 'SequelizeValidationError'){
-                    err.errors.forEach( (e) => (error[e.path] = e.message))
+                    err.errors.forEach( (e) => (errors[e.path] = e.message))
                 }
-                throw new UserInputError("Bad input", { error });
+                throw new UserInputError("Bad input", { errors });
             }
         }
     }
